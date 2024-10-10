a0d3ffd205bf94345b85c10cf3cb71fdcf3520ac
Heryad <heryad.me@gmail.com>
2024-10-10 10:30:21 +0400

    send commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,6 @@ import { useEffect } from "react";
 import { useRouter } from 'next/navigation'
 import WebApp from '@twa-dev/sdk'
 
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: string;
-}
-
 export default function Home() {
   const router = useRouter()
 
@@ -24,17 +15,19 @@ export default function Home() {
       day: 'numeric'
     })
 
-    if (WebApp.initDataUnsafe.user) {
-      const catResp = await fetch('/dashboard/api/Login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ telegramID: WebApp.initDataUnsafe.user?.id, userName: WebApp.initDataUnsafe.user?.username, userFirstName: WebApp.initDataUnsafe.user?.first_name, registerDate: formattedDate, userBalance: '0',  invitedBy: WebApp.initDataUnsafe?.start_param})
-      });
-      if(catResp != null){
-        const mCatData = await catResp.json();
-        localStorage.setItem('userID', mCatData.data[0].telegramID);
-        localStorage.setItem('userName', mCatData.data[0].userFirstName);
-        router.push('/dashboard')
+    if (typeof window !== "undefined") {
+      if (WebApp.initDataUnsafe.user) {
+        const catResp = await fetch('/dashboard/api/Login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ telegramID: WebApp.initDataUnsafe.user?.id, userName: WebApp.initDataUnsafe.user?.username, userFirstName: WebApp.initDataUnsafe.user?.first_name, registerDate: formattedDate, userBalance: '0',  invitedBy: WebApp.initDataUnsafe?.start_param})
+        });
+        if(catResp != null){
+          const mCatData = await catResp.json();
+          localStorage.setItem('userID', mCatData.data[0].telegramID);
+          localStorage.setItem('userName', mCatData.data[0].userFirstName);
+          router.push('/dashboard')
+        }
       }
     }
   }
